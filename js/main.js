af916c13a115789a61a7e4049b41117789dda1a1
learntips0507 <learntips0507@gmail.com>
2020-03-07 19:31:51 +0600

    MD Bank LTD has been completed

--- js/main.js
+++ js/main.js
@@ -1,20 +1,58 @@
-        //login button set with javascript
-        const btnLogin = document.getElementById("login");
-        btnLogin.addEventListener("click",function(){
-            const loginArea = document.getElementById("login-area");
-            loginArea.style.display = "none";
-            const transitionArea = document.getElementById("transition-area");
-            transitionArea.style.display = "block";
-        })
-        //deposit set with javascript
-        const depositAmount = document.getElementById("depositBtn");
-        depositAmount.addEventListener("click", function(){
-            const depositNumber = document.getElementById("depositAmountId").value;
-            const depositAccurateNumber = parseFloat(depositNumber);
+//login form Area Dynamic 
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click",function(){
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display = "none";
+    const transitionArea = document.getElementById("transition-area");
+    transitionArea.style.display = "block";
+})
 
-            const currantDeposit = document.getElementById("currantDepositId").innerText;
-            const currentDepositNumber = parseFloat(currantDeposit);
-            const totalDeposit = currentDepositNumber + depositAccurateNumber;
-            document.getElementById("currantDepositId").innerText = totalDeposit;
-            document.getElementById("depositAmountId").value = "";
-        })
+//Deposit Area with Total Balance Dynamic
+const depositBtn = document.getElementById("depositBtn");
+depositBtn.addEventListener("click",function(){
+    //Deposit Input button
+    const depositAmountId = document.getElementById("depositAmountId").value;//input
+    const depositNumber = parseFloat(depositAmountId);
+
+    //add with balance
+    const currantDepositId = document.getElementById("currantDepositId").innerText;//span
+    const currentdepositNumber = parseFloat(currantDepositId);
+    //total deposit
+    const totaldeposit = depositNumber + currentdepositNumber;
+    document.getElementById("currantDepositId").innerText = totaldeposit;
+
+    //Total balance with Current Deposit
+    const balanceAmount = document.getElementById("balanceAmount").innerText;
+    const balanceNumber = parseFloat(balanceAmount);
+    //add with current balance
+    const totalBalance =  balanceNumber + depositNumber;
+    document.getElementById("balanceAmount").innerText = totalBalance;
+
+
+    document.getElementById("depositAmountId").value = "";
+
+})
+
+//Withdraw money with reduce or Total balance 
+const withdrawBtn = document.getElementById("withdrawBtn");
+withdrawBtn.addEventListener("click", function(){
+    //Withdraw Button
+    const withdrawAmount = document.getElementById("withdrawAmount").value;//input
+    const withdrawNumber = parseFloat(withdrawAmount);
+    //add withdraw current money
+    const withdrawBtnAmount = document.getElementById("withdrawBtnAmount").innerText;//span
+    const withdrawMoney = parseFloat(withdrawBtnAmount);
+    //total withdraw
+    const totalWithdraw = withdrawNumber +  withdrawMoney;
+    document.getElementById("withdrawBtnAmount").innerText = totalWithdraw;
+
+
+    //add with total balance
+    const reduceBalance = document.getElementById("balanceAmount").innerText;//span
+    const reduceNumber = parseFloat(reduceBalance);
+    const reduceTotalBalance = reduceNumber - withdrawNumber;
+    document.getElementById("balanceAmount").innerText = reduceTotalBalance;
+
+    document.getElementById("withdrawAmount").value ="";
+})
+
